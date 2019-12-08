127609134065b07ccf1e0eb8efd361f0d31e4bf6
marjoriernunes <marjorie.rnunes@gmail.com>
2019-12-08 19:18:07 -0300

    create the logic for increment/decrement the count

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }))
+  }
+
+  decrement = () => {
+    this.setState(({ count }) => ({
+      count: count - 1
+    }))
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="counter">{this.state.count}</h2>
+        <button className="increment" onClick={this.increment}>Increment</button>
+        <button className="decrement" onClick={this.remove}>Decrement</button>
+      </div>
+    );
+  }
 }
 
 export default App;
